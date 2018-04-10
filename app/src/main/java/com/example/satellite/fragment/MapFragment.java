48196c63982742e8e3b7d27b81baa88575e72116
tulipper <1324227851@qq.com>
2018-04-10 10:33:40 +0800

    加入百度地图api

---
+++ app/src/main/java/com/example/satellite/fragment/MapFragment.java
@@ -0,0 +1,10 @@
+package com.example.satellite.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Administrator on 2018/4/10.
+ */
+
+public class MapFragment extends Fragment {
+}
